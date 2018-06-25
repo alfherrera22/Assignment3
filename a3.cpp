d7f32668ce56af026ce314d7dc798564c9baa736
Alfonso Herrera <alf.herrera.22@gmail.com>
2018-06-25 16:14:54 -0700

    Created the work funtion that will create the childs

--- a3.cpp
+++ a3.cpp
@@ -1,16 +1,53 @@
 /*
- * 
- * 
- */
-
-#inclide "a3.h"
+    Assignment 3 CS 570
+    June 27th 2018
+    Guy Leonard
+    Pierre Kharat
+    Alfonso Herrera
+*/
+ 
+#include "a3.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]){
 	
+	if(argc == 1){//Use default 25 sec
+		
+		work(STANDARD_TIME);
+		
+	}
 	
+	else if (argc > 1){
+		
+		int sec;
+		
+		if((sec = atoi(argv[1])) == 0){
+			cout << "Invalid number. Program will exit"<< endl;
+			return 0;
+		}
+		
+		else
+			work(sec);
+	}
 	
+	return 0;
+}
+
+
+int work(int sec){
+
+	clock_t start;
+	
+    double duration;
+
+    start = clock();
+    
+    sleep(sec);
+    
+	duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;	
+	
+	cout << duration << "\nThank you and good bye" << endl;
 	
 	return 0;
 }
--- a3.h
+++ a3.h
@@ -1,3 +1,13 @@
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <stdio.h> 
+
+//If user does not input anything we will use 25 sec
+#define STANDARD_TIME 25
+
+int work(int sec);
