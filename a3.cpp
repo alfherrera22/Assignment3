e92dc5a76536115887bc298d4e726feede1e5736
Alfonso Herrera <alf.herrera.22@gmail.com>
2018-06-25 16:43:21 -0700

    Created the child process but still need to figure out how to do stuff

--- a3.cpp
+++ a3.cpp
@@ -12,11 +12,8 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 	
-	if(argc == 1){//Use default 25 sec
-		
-		work(STANDARD_TIME);
-		
-	}
+	//Use default 25 sec if no argument
+	if(argc == 1) { work(STANDARD_TIME); }
 	
 	else if (argc > 1){
 		
@@ -38,16 +35,61 @@ int main(int argc, char* argv[]){
 int work(int sec){
 
 	clock_t start;
-	
     double duration;
-
-    start = clock();
-    
-    sleep(sec);
-    
-	duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;	
 	
-	cout << duration << "\nThank you and good bye" << endl;
+	pid_t PID, PID2;
+	
+	
+	/* This child process should notify the other child
+	 * upon reaching the specified number of seconds 
+	 */
+	
+	PID = fork();
+	
+	if(PID == 0){ //Child proccess executes code here
+		
+		/* This second child prints the hour, minute, and second 
+		 * of the time of day to the user terminal once every 
+		 * second 
+		 */
+		PID2 = fork();
+		
+		if(PID == 0){ //Child process executes code here
+			
+			
+		}
+		
+		else if(PID > 0){//Parent process executes code here
+			
+		
+		}
+		
+		else{
+			perror("fork() error");
+			exit(-1);		
+		} // End if
+		
+	}
+
+	else if(PID > 0){ //Parent process executes code here
+		
+		cout << "\nHave a nice day" << endl;
+	}
+
+	else{
+		perror("fork() error");
+		exit(-1);		
+	} // End if
+
+
+    //start = clock();
+    //sleep(sec);
+    
+	//duration = ( clock() - start ) / CLOCKS_PER_SEC;	
+	
+	//printf("Duration = %4.2f\n",duration);
+	
+	
 	
 	return 0;
 }
--- a3.h
+++ a3.h
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <cstdlib>
-#include <stdio.h> 
+#include <stdio.h>
 
 //If user does not input anything we will use 25 sec
 #define STANDARD_TIME 25
