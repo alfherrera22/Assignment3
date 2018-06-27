39cb904a0120a639f285bc94e79fbb039015582c
Alfonso Herrera <alf.herrera.22@gmail.com>
2018-06-26 17:16:21 -0700

    README updated and also changed the 'busy waiting' inside of the seek_Time() function so it will use sleep() instead of while loop. Ready for submission.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Assignment3
-CS 570 Summer class
+Pierre Kharat: cssc1197
+Alfonso Herrera: cssc1155
+
+CS 570 Summer 2018, Assignment #3 Programmable Event and Timer Control Program
+
+
+README FILE
+
+
+File Manifest
+
+- README.txt: This is the current text file being read which will provide instructions
+	      for the program.
+
+- makefile:   This makefile is the one that will run and link our code creating an 
+	      executable file.
+
+- a3.h: Contains all the function prototypes and includes needed for the program to run.
+
+- a3.cpp: This is the main file for the whole project. This will allow us to run the Programmable Event and Timer Control Program.
+
+Compile Instructions
+
+- We included, inside of the 'makefile', the 'gcc' command that will compile the 
+  source code found inside the already mentioned 'makefile'.
+
+Operating Instructions
+
+- First, we need to execute the 'makefile' with the 'make' command. Then, after the
+  executable is created, we need to run this executable and pass one argument. 
+  
+Design Decisions
+
+- We decided to include two functions plus the main function to better optimize the project
+ 
+ Work organization
+ 
+- Alfonso Herrera: Created the makefile and README file. Worked on getting the pthreads
+  working as well as error checking.
+
+
+- Pierre Kharat: Created the functions and was in charge of getting the date and time displayed
+  to the screen.
--- a3.cpp
+++ a3.cpp
@@ -10,29 +10,44 @@
 
 using namespace std;
 
+// Pthreads
 pthread_t COUNT,PRINT;
 
+// These variables will take care of the time
 time_t rawtime;
 time_t endingtime;
 struct tm * timeinfo;
 struct tm * endinginfo;
+long sec;
+		
 
-//Signal that will notify the PRINT thread to terminate
+// Signal that will notify the PRINT thread to terminate
 bool sig = true;
 
+
+/** 
+ * This is the main function of the program. The program is in charge of 
+ * creating two threads. Each thread will call a different function, seek_Time()
+ * and print_Time() respectively. If no argument entered by the user the 
+ * program will use the standard 25 seconds. If there is an argument, first 
+ * the program will make sure if it is a correct input and if it is it will
+ * send the amount of seconds entered.
+ * 
+ */
 int main(int argc, char* argv[]){
 	
 	time(&rawtime);
   	timeinfo = localtime(&rawtime);
-	//printf("Duration = %s", asctime(timeinfo));
-	
-	//Use default 25 sec if no argument
+  	
+	// Use default 25 sec if no argument
 	if(argc == 1){ 
+		
+		sec = STANDARD_TIME;
 		
 		// Adds 25 seconds to local time;
 		// To be used for while loop
 		endingtime = mktime(timeinfo);
-		endingtime += STANDARD_TIME;
+		endingtime += sec;
 		endinginfo = localtime(&endingtime);
 		
 		pthread_create(&COUNT, NULL, &seek_Time, NULL);
@@ -45,8 +60,7 @@ int main(int argc, char* argv[]){
 	
 	else if (argc > 1){
 		
-		long sec;
-		
+		// Error checking
 		if((sec = atoi(argv[1])) == 0){
 			cout << "Invalid number. Program will exit"<< endl;
 			return 0;
@@ -74,34 +88,48 @@ int main(int argc, char* argv[]){
 	return 0;
 } // End main
 
-
+/**
+ * This function will busy wait until the desired time is reached. The way
+ * in which we check the time is by using the time() function and adding
+ * the amount of seconds that the user previously input.
+ * 
+ */
 void *seek_Time(void *i){
 	
   	time(&rawtime);
   	timeinfo = localtime(&rawtime);
 	
-	while(mktime(timeinfo) != endingtime);
+	// Busy waiting until amount of seconds reached
+	sleep(sec);
 	
+	// When amount of seconds reached we signal the other program to exit
 	sig = false;
-
+	
 	pthread_exit(NULL);
 } // End *seek_Time()
 
+/**
+ * This function will print the current time every single second until the 
+ * function seek_Time() changes the signal from true to false. After this
+ * the program will gracefully exit.
+ * 
+ */
 
 void *print_Time(void *i){
 	
   	time(&rawtime);
   	timeinfo = localtime(&rawtime);
 	
-	
 	// While loop prints current time until
 	// Stopping time is reached
 	
 	while(sig) {
+		
 		time(&rawtime);
 		timeinfo = localtime(&rawtime);
-		printf("Duration = %s", asctime(timeinfo));
-		sleep(1);
+		printf("%s", asctime(timeinfo));
+		sleep(1); // sleep(1) so the loop will only print each second
+		
 	} // End while
 	
 	
